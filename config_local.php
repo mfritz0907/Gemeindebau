9d282929cbffdb9f4dad9ee5616ff1ace96be733
mfritz0907 <mf@martinfritz.net>
2025-12-07 14:42:32 +0100

    Add files via upload

---
+++ config_local.php
@@ -0,0 +1,10 @@
+<?php
+// Database configuration settings
+
+define('DB_HOST', 'db5012502939.hosting-data.io');
+define('DB_USER', 'dbu5509889');
+define('DB_PASSWORD', '@Ratnitz11');
+define('DB_NAME', 'dbs10511209');
+define('GOOGLE_MAPS_API_KEY', 'AIzaSyD5yAr9V_HbvE9EIbFckCjA1p5u0XgZYlc');
+
+?>
